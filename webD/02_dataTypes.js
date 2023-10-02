5ccfd0cfbfa06fd728a28d9d6bb857a91a76076b
Krishna0302 <133387126+Krishna0302@users.noreply.github.com>
2023-10-02 20:12:06 +0000

    dataTypes

---
+++ webD/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)  we are using nodeJS, not browser
+
+let name = "krishna";
+let isAccount = true;
+let a = 3;
+
+/*premitive dataTypes:
+number => 2 to power 53
+bigint => for bigger values
+string => ""
+boolean => true/false
+null => standalone and object
+undefined => 
+symbol => unique */
+
+// object
+
+console.log(typeof isAccount);
