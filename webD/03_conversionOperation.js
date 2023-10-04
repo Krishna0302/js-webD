8470da15c5835341eff409d65f744e8be87427ca
Krishna0302 <133387126+Krishna0302@users.noreply.github.com>
2023-10-04 17:45:19 +0000

    operation

--- webD/03_conversionOperation.js
+++ webD/03_conversionOperation.js
@@ -4,11 +4,27 @@ let age = "20abc";
 let num = Number(a);
 let num2 = Number(age);
 
-console.log(typeof(score));
-console.table([typeof score, typeof age, typeof num, typeof a]);
-console.log(num); // NaN => Not a Number
-console.log(num2);
+// console.log(typeof(score));
+// console.table([typeof score, typeof age, typeof num, typeof a]);
+// console.log(num); // NaN => Not a Number
+// console.log(num2);
 
 // "20" => 20
 // "20abc" => NaN
 // true => 1; false => 0
+
+let b = "krishna"
+let isTrue = Boolean(b)
+console.log(isTrue)
+
+// 1 => true, 0 => false
+// "" => false, "krishna" => true
+
+// ****************Operation*****************
+console.log(2+2)
+console.log(2**3)
+console.log(2%3)
+let str1 = "krishna"
+let str2 = "bairwa"
+let str3 = str1 + str2
+console.log(str3)
