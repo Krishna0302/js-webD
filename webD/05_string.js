480e32a473f2b4fc4a4db621eb34bebcd0a6b30d
Krishna0302 <133387126+Krishna0302@users.noreply.github.com>
2023-10-05 18:29:08 +0000

    string

---
+++ webD/05_string.js
@@ -0,0 +1,27 @@
+const name = "krishna"
+const repoCount = 50
+// console.log('my name is ${name} and my repo count is ${repoCount}')
+
+// const name = new String('krishna')
+// console.log(name[0])                 // to access each element by its index
+// console.log(name.length)            // String length
+
+console.log(name.toUpperCase())
+console.log(name.charAt(2))
+console.log(name.indexOf('r'))
+
+const newName = name.substring(0,4)
+console.log(newName)
+
+const anotherName = name.slice(-6,4)
+console.log(anotherName)
+
+const newString = "     krishna     "
+console.log(newString.trim())
+
+const url = "https://krishna.com/krishna%20Bairwa"
+console.log(url.replace('%20', '--'))
+
+console.log(url.includes("krishna"))
+
+console.log(name.split('-'))
