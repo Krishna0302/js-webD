dfc49f9a90b7542609fcddc4cbcbb16f10e6504b
Krishna0302 <133387126+Krishna0302@users.noreply.github.com>
2023-10-07 17:18:32 +0000

    array1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+// const arr =[1, 2, 3, 4, 5, "krishna"]
+// console.log(arr.length)
+// console.log(arr[3])
+const arr2 = new Array(1, 2, 3, 4, 5)
+
+// **********************************__arrays method__***********************************
+
+// arr2.push(6)
+// arr2.push(7)
+// arr2.pop()    // remove last element of array
+
+// arr2.unshift(2)    // add element at the 0th index of array
+// arr2.shift()          // remove first element 
+
+// console.log(arr2.includes(4))
+// console.log(arr2.indexOf(3))
+
+const arr3 = arr2.join()    // datatype will be string
+// console.log(arr3)
+// console.log(arr2)    
+
+console.log(arr2.slice(1,3))
+console.log(arr2)
+console.log(arr2.splice(1,3))
+console.log(arr2)
+
