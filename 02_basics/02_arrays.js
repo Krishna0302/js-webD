724a006829d4deb1adab408df45229552bda0b6d
Krishna0302 <133387126+Krishna0302@users.noreply.github.com>
2023-10-14 05:15:13 +0000

    array2

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,19 +1,21 @@
 const marvelHeros = ["ironMan", "thor", "spiderMan"]
 const dc = ["superMan", "flash", "batMan"]
+const hh = ["ShaktiMan", "hero"]
 // marvelHeros.push(dc)
 // console.log(marvelHeros)
-// console.log(marvelHeros[3][1])
+// console.log(marvelHeros[2][1])
 
 // const allHeros = marvelHeros.concat(dc)   // can merge only two arrays
 // console.log(allHeros)
 
-const allNewHeros = [...marvelHeros, ...dc]    // can merge more than two arrays
-console.log(allNewHeros)
+const allNewHeros = [...marvelHeros, ...dc, ...hh]    // can merge more than two arrays
+// console.log(allNewHeros)
 
-// const arr = [1, 2, 3, [5, 6, 7], [7, 8, [0, 1, 2]]]
+const arr = [1, 2, 3, [5, 6, 7], [7, 8, [0, 1, 2]]]
+// console.log(arr)
 // const arr1 = arr.flat(Infinity)
-// console.log(arr1)
+// console.log(arr.flat(Infinity))
 
 console.log(Array.isArray("krishna"))
 console.log(Array.from("krishna"))
-console.log(Array.from({name: "krishna"}))   // imp for interviews
+console.log(Array.from({name: "krishna"}))   // imp for interviews, we will have to tell compiler which one to make Array (key or value).
