3de52df90e2bdffc9dbdb32ffd03208540d58c58
Krishna0302 <133387126+Krishna0302@users.noreply.github.com>
2023-10-14 06:34:46 +0000

    constructor

--- 02_basics/04_object_constructor.js
+++ 02_basics/04_object_constructor.js
@@ -7,22 +7,22 @@ tinderUser.name = "krishna"
 tinderUser.isLoggedIn = false;
 // console.log(tinderUser)
 
-const newUser = {
-    email: "krishna@gmail.com",
-    fullName: {
-        userFullName: {
-            firstName: "krishna",
-            lastName: "bairwa"
-        }
-    }
-}
-// console.log(newUser.fullName.userName)
+// const newUser = {
+//     email: "krishna@gmail.com",
+//     fullName: {
+//         userFullName: {
+//             firstName: "krishna",
+//             lastName: "bairwa"
+//         }
+//     }
+// }
+// console.log(newUser.fullName.userFullName)
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "c", 4: "d"}
 // const obj3 = {obj1, obj2}
 // const obj3 = Object.assign({}, obj1, obj2)
-const obj3 = {...obj1, ...obj2}
+// const obj3 = {...obj1, ...obj2}
 // console.log(obj3)
 
 const arr = [
@@ -49,7 +49,7 @@ const course = {
 const {courseInstructor: instructor} = course   //Destucturing
 // console.log(instructor)
 
-//API   
+//API   will be treated ad JSON
 // {
 //    "name": "krishna",
 //    "id" : "k@gmail.com",
