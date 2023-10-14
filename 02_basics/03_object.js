412fed3bcb2a047ba472286f9a0dd7abdba905c8
Krishna0302 <133387126+Krishna0302@users.noreply.github.com>
2023-10-14 05:39:13 +0000

    object

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -4,20 +4,27 @@ const mySym = Symbol("key1")
 const user = {
     name: "krishna",
     [mySym]: "myKey",
-    age: 21,
+    age:"21",
     email: "krishna@gmail.com",
     location: "bhopal"
 }
-// console.log(user["email"])
 // console.log(user.email)
-// console.log(user["mySym"])
+// console.log(user["email"])
+// console.log(user["age"])
+// console.log(user[mySym])
+// console.log(typeof user[mySym])
+Object.freeze(user)                    // changes will not reflect
 user.email = "bairwa@gmail.com"
-// Object.freeze(user)   // changes will not reflect
+console.log(user["email"])
 
-user.greeting = function(){
-    console.log("hello JS user")
+// user.greeting = function(){
+//     console.log("hello JS user")
+// }
+// user.greeting2 = function(){
+//     console.log('hello JS user, ${this.name}');
+// }
+
+const greeting = function(){
+    console.log("hello user")
 }
-user.greeting2 = function(){
-    console.log('hello JS user, ${this.name}');
-}
-console.log(user.greeting2())
+console.log(greeting())
