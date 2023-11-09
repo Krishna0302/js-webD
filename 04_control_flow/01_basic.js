25294e6ee01d801e8f5e799b92c45de5a6e2f3ea
Krishna0302 <133387126+Krishna0302@users.noreply.github.com>
2023-11-09 07:10:21 +0000

    basic control flow

---
+++ 04_control_flow/01_basic.js
@@ -0,0 +1,14 @@
+// if statements
+// if(2!=3){
+//     console.log("executed")
+// }
+
+// ==, ===, !==, >, <, =>, =<, !=
+const score = 200
+// if(score > 100){
+//     const power = "fly"
+//     console.log("user power: "+ power)
+// }
+
+const balance = 10000
+if(balance > 5000) console.log("agreed"), console.log("test");  // implicit scope
