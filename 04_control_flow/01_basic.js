dd499284559ab225bb8be33ef260d233e3c661b5
Krishna0302 <133387126+Krishna0302@users.noreply.github.com>
2023-11-09 13:56:47 +0000

    basic control flow

--- 04_control_flow/01_basic.js
+++ 04_control_flow/01_basic.js
@@ -12,3 +12,14 @@ const score = 200
 
 const balance = 10000
 if(balance > 5000) console.log("agreed"), console.log("test");  // implicit scope
+
+// Nullish Coalescing Operator (??): null undefined
+let val1
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+// console.log(val1)
+
+// Ternary operator
+condition ? true : false
